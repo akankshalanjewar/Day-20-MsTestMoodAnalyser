dcde6f6bdcc3ef00f5549cef0364326e5436f83b
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-04-02 20:44:42 +0530

    [Akanksha]: mood analyser sad

---
+++ MoodAnalyser/MoodAnalyse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+     public class MoodAnalize
+        {
+            private string message;
+
+            public MoodAnalize(string message)
+            {
+                this.message = message;
+            }
+
+            public string AnalizeMood()
+            {
+                if (this.message.Contains("SAD"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+        }
+    }
+
+
---
+++ MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in SAD Mood";
+            MoodAnalize mood = new MoodAnalize(message);
+
+            // Act
+            string mood1 = mood.AnalizeMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood1);
+        }
+    }
+}
+        
+    
+
